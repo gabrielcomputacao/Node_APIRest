3836e53b36b7f26c31725c26aea86dcb6a914221
gabriel <gabrielcomputacao@outlook.com>
2024-01-26 08:43:03 -0300

    feat:  configuring cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,44 +2,78 @@ import { FastifyInstance } from "fastify";
 import { knex } from "../database";
 import { randomUUID } from "crypto";
 import { z } from "zod";
+import { CheckSessionIdExists } from "../middlewares/check-session-id-exist";
+
+//  todo: Cookies -> formas da gente manter o contexto entre requisições
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const transactions = await knex("transactions").select();
+  app.get(
+    "/",
+    // todo: preHandler sao funcoes que sao executadas antes de executar o handler, para verificações ou autenticações
+    {
+      preHandler: [CheckSessionIdExists],
+    },
+    async (request, response) => {
+      const { sessionId } = request.cookies;
 
-    /* 
+      const transactions = await knex("transactions")
+        .where("session_id", sessionId)
+        .select();
+
+      /* 
         Trabalhar com objetos é melhor para questao de resposta e requisicao(envio)
         mais facil adicionar e remvoer informações no futuro
     */
-    return { transactions };
-  });
+      return { transactions };
+    }
+  );
 
-  app.get("/:id", async (request) => {
-    const getTransactionsParamsSchema = z.object({
-      id: z.string().uuid(),
-    });
+  app.get(
+    "/:id",
+    {
+      preHandler: [CheckSessionIdExists],
+    },
+    async (request) => {
+      const getTransactionsParamsSchema = z.object({
+        id: z.string().uuid(),
+      });
 
-    const { id } = getTransactionsParamsSchema.parse(request.params);
+      const { id } = getTransactionsParamsSchema.parse(request.params);
 
-    /* 
+      const { sessionId } = request.cookies;
+
+      /* 
         funcao first informa ao knex que é apenas um dado que vai ser retorna, assim ele nao retorna um array
     */
-    const transaction = await knex("transactions").where("id", id).first();
+      const transaction = await knex("transactions")
+        .where("id", id)
+        .andWhere("session_id", sessionId)
+        .first();
 
-    return { transaction };
-  });
+      return { transaction };
+    }
+  );
 
-  app.get("/summary", async () => {
-    /* 
+  app.get(
+    "/summary",
+    {
+      preHandler: [CheckSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
+
+      /* 
         sem o first o knex retorna um array
     */
 
-    const summary = await knex("transactions")
-      .sum("amount", { as: "amount" })
-      .first();
+      const summary = await knex("transactions")
+        .sum("amount", { as: "amount" })
+        .where("session_id", sessionId)
+        .first();
 
-    return { summary };
-  });
+      return { summary };
+    }
+  );
 
   app.post("/", async (request, response) => {
     const createTransactionBodySchema = z.object({
@@ -53,10 +87,33 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body
     );
 
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      /* 
+            Salva nos cookie o nome do cookie e o dado que foi criado como um uuiD nesse caso
+        */
+      response.cookie("sessionId", sessionId, {
+        /* rotas que podem acessar esse cookie dentro do backend */
+        path: "/",
+        /* tem que passar em segundos segundo a documentação do fastify para falar o tempo de expiração do cookie 
+
+                1000 milisegundos sao 1 segundos
+                60 segundos sao 1 minutos
+                60 minutos sao 1 hora
+                24 horas sao 1 dia
+            */
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
     await knex("transactions").insert({
       id: randomUUID(),
       titlle: title,
       amount: type === "credit" ? amount : amount * -1,
+      session_id: sessionId,
     });
 
     return response.status(201).send();
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,22 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
+import crypto from "node:crypto";
+import { transactionsRoutes } from "./routes/transactions";
 
-import crypto from 'node:crypto'
-import { transactionsRoutes } from './routes/transactions'
+const app = fastify();
 
-const app = fastify()
+// ! É importante a ordem para se trabalhar com cookie, instanciar antes das rotas que o cookie vai ser trabalhado
+
+app.register(cookie);
 
 app.register(transactionsRoutes, {
-  prefix: 'transactions'
-})
-
+  prefix: "transactions",
+});
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP Server Running')
-  })
-
-
+    console.log("HTTP Server Running");
+  });
