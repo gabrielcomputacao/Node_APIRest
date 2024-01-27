9516692360d6d5c57ccd7efd78482c8d1e67b706
gabriel <gabrielcomputacao@outlook.com>
2024-01-27 07:49:43 -0300

    feat:  creating first test with vitest

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
+import { transactionsRoutes } from "./routes/transactions";
+
+export const app = fastify();
+
+// ! É importante a ordem para se trabalhar com cookie, instanciar antes das rotas que o cookie vai ser trabalhado
+
+/* 
+  todo: Aqui seria um preHandler global para todas as rotas
+
+  app.addHook("preHandler", async (request, response) => {
+    console.log("prehandler global");
+  });
+
+*/
+
+app.register(cookie);
+
+app.register(transactionsRoutes, {
+  prefix: "transactions",
+});
